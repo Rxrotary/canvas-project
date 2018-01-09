83bad15967ae5896017126d6c9461d0c6cd6524c
allenchum <33272318+allenchum@users.noreply.github.com>
2018-01-09 16:38:25 +0800

    color.current

--- Project2ndD/drawing_rectangle.js
+++ Project2ndD/drawing_rectangle.js
@@ -6,13 +6,13 @@ class DrawingRectangle extends PaintFunction{
     }
     
     onMouseDown(mouse,event){
-        this.contextReal.fillStyle = color.primary;
+        this.contextReal.fillStyle = color.current;
         this.contextReal.lineStyle = color.secondary;
         this.contextReal.lineWidth = size.x * 0.1;
         this.orig = mouse;      //mouse = {x:   , y:   } , the coordinate of mouse event
     }
     onDragging(mouse,event){
-        this.contextDraft.fillStyle = color.primary;
+        this.contextDraft.fillStyle = color.current;
         this.contextDraft.lineStyle = color.secondary;
         this.contextReal.lineWidth = size.x * 0.1;
         this.contextDraft.clearRect(0,0,canvasReal.width,canvasReal.height);
